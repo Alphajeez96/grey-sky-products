cb17f9761d161b607027ca9138010f99c832f624
alphajeez <chimeremezechukwudire@gmail.com>
2024-02-15 23:34:42 +0100

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        black: '#252526',
+        'primary-green': '#377364',
+        'primary-blue': '#375673'
+      },
+      fontFamily: {
+        inter: 'Inter, sans-serif',
+        bitter: 'Bitter, serif'
+      }
+    }
+  },
+  plugins: []
+}
